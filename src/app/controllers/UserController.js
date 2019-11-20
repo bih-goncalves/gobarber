80b158c6460493863e12a034430f2f62ef2656b6
bih_goncalves <bih_goncalves@hotmail.com>
2019-11-20 19:42:02 -0300

    chorus: testing token from session

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -21,6 +21,7 @@ class UserController {
   }
 
   async update(req, res) {
+    console.log(req.userId);
     return res.json({ ok: true });
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,20 @@
-import { Router } from 'express';
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+import { Router } from "express";
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+
+import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
-routes.put('/users', UserController.update);
+// routes without session
+// new user
+routes.post("/users", UserController.store);
+// login
+routes.post("/sessions", SessionController.store);
 
-routes.post('/sessions', SessionController.store);
+// routes with session
+routes.use(authMiddleware);
+// update user info
+routes.put("/users", UserController.update);
 
 export default routes;
