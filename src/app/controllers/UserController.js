ce66638b89122836d055a8bc6be6fa957e43f34d
bih_goncalves <bih_goncalves@hotmail.com>
2019-11-21 01:29:21 -0300

    feature: validation of info received on user route

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,16 +1,28 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    // validate data for creating user
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    // check if user already exists
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
-
+    // creating user
     const {
- id, name, email, provider 
-} = await User.create(req.body);
+      id, name, email, provider,
+    } = await User.create(req.body);
 
     return res.json({
       id,
@@ -21,6 +33,19 @@ class UserController {
   }
 
   async update(req, res) {
+    // validate data before update user info
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string().min(6).when('oldPassword', (oldPassword, field) => (oldPassword ? field.required() : field)),
+      confirmPassword: Yup.string().min(6).when('password', (password, field) => (password ? field.required().oneOf([Yup.ref('password')]) : field)),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    // get data from form
     const { email, oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
-import { Router } from "express";
-import UserController from "./app/controllers/UserController";
-import SessionController from "./app/controllers/SessionController";
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
-import authMiddleware from "./app/middlewares/auth";
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 // routes without session
 // new user
-routes.post("/users", UserController.store);
+routes.post('/users', UserController.store);
 // login
-routes.post("/sessions", SessionController.store);
+routes.post('/sessions', SessionController.store);
 
 // routes with session
 routes.use(authMiddleware);
 // update user info
-routes.put("/users", UserController.update);
+routes.put('/users', UserController.update);
 
 export default routes;
