c6f7839459f2fd0dc70f8658ed8f7831b9d29ead
bih_goncalves <bih_goncalves@hotmail.com>
2019-11-23 17:19:46 -0300

    feature: using multer to send files to server

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // routes without session
 // new user
@@ -16,5 +20,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 // update user info
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => res.json({ ok: true }));
 
 export default routes;
