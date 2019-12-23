a8ad10b5804c3497125c9d541b95b3cf26bd1c91
bih_goncalves <bih_goncalves@hotmail.com>
2019-12-23 17:40:39 -0300

    chore: displaying static files

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -6,8 +6,12 @@ class UserController {
     // validate data for creating user
     const schema = Yup.object().shape({
       name: Yup.string().required(),
-      email: Yup.string().email().required(),
-      password: Yup.string().required().min(6),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
     });
 
     if (!(await schema.isValid(req.body))) {
@@ -20,9 +24,7 @@ class UserController {
       return res.status(400).json({ error: 'User already exists.' });
     }
     // creating user
-    const {
-      id, name, email, provider,
-    } = await User.create(req.body);
+    const { id, name, email, provider } = await User.create(req.body);
 
     return res.json({
       id,
@@ -38,8 +40,16 @@ class UserController {
       name: Yup.string(),
       email: Yup.string().email(),
       oldPassword: Yup.string().min(6),
-      password: Yup.string().min(6).when('oldPassword', (oldPassword, field) => (oldPassword ? field.required() : field)),
-      confirmPassword: Yup.string().min(6).when('password', (password, field) => (password ? field.required().oneOf([Yup.ref('password')]) : field)),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field,
+        ),
+      confirmPassword: Yup.string()
+        .min(6)
+        .when('password', (password, field) =>
+          password ? field.required().oneOf([Yup.ref('password')]) : field,
+        ),
     });
 
     if (!(await schema.isValid(req.body))) {
