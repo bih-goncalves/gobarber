446145d21fcd7769ac1d1f057a1d823991398ef7
bih_goncalves <bih_goncalves@hotmail.com>
2019-12-23 17:39:03 -0300

    feature: providers to backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
   env: {
     node: true,
-    es6: true,
+    es6: true
   },
   extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
     'class-methods-use-this': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
     'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-  },
+    quotes: ['error', 'single']
+  }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -21,6 +22,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 // update user info
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
