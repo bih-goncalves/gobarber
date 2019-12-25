3fdf730253e773a0d9a63661533a528010f13f8e
bih_goncalves <bih_goncalves@hotmail.com>
2019-12-25 15:36:58 -0300

    feature: listing appointments

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -27,6 +27,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 //create appointment
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 // store file like avatar
 routes.post('/files', upload.single('file'), FileController.store);
 
