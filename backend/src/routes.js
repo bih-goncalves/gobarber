a06b78d0889f64f6b54a5a4264ab599a3ee527f2
bih_goncalves <bih_goncalves@hotmail.com>
2019-12-25 16:03:40 -0300

    feature: list schedule for specific day

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-import AppointmentController from './app/controllers/AppointmentController';
+import SessionController from './app/controllers/SessionController';
+import ScheduleController from './app/controllers/ScheduleController';
+import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -29,6 +30,8 @@ routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
 // store file like avatar
+routes.get('/schedule', ScheduleController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
