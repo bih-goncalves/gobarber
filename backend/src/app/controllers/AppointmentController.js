2d106db6cba8cec75c3e4c7ca8e7404b861071d3
bih_goncalves <bih_goncalves@hotmail.com>
2019-12-25 15:09:06 -0300

    chore: finishing some checks for new appointments

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,7 +1,8 @@
-import Appointment from '../models/Appointment';
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointment from '../models/Appointment';
+import User from '../models/User';
 
 class AppointmetController {
   async store(req, res) {
@@ -27,6 +28,26 @@ class AppointmetController {
       return res
         .status(401)
         .json({ error: 'You can only create appointments with providers' });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+    // verify if the date is in the future
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not allowed' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    // check availability from provider
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
     }
 
     const appointment = await Appointment.create({
