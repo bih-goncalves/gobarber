ec00000faeaf221fff8824bbde39ebbd01bc37ff
bih_goncalves <bih_goncalves@hotmail.com>
2019-12-25 18:50:53 -0300

    feature: mailtrap configuration

---
+++ backend/src/config/mail.js
@@ -0,0 +1,13 @@
+// credentials used from mailtrap to test it
+export default {
+  host: '	smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'bcfa277b82d5e3',
+    pass: '6d05417af353f1',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
