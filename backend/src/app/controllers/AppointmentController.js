3bba28510bb6e2ff12c882f6722da52d28bc5ca3
bih_goncalves <bih_goncalves@hotmail.com>
2019-12-25 17:44:59 -0300

    feature: notification on appointment

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmetController {
   async index(req, res) {
@@ -83,6 +85,22 @@ class AppointmetController {
       user_id: req.userId,
       provider_id,
       date,
+    });
+
+    // get user info to nitification
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      {
+        locale: pt,
+      },
+    );
+
+    // notify provider
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
